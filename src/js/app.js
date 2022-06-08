aff7f2378de6d4a5361ea3fdb78d7ad79f7233e3
Maksym <bilanych.max98009@gmail.com>
2022-06-08 14:01:54 +0300

    add js form dropmenu items

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 import ProfileDropMenu from "./modules/profile-drop-menu.js";
+import ItemsActive from './modules/items-active.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
@@ -10,4 +11,6 @@ window.addEventListener('DOMContentLoaded', () => {
   new ProfileDropMenu('.button-profile_disconnected', '.disconnected-menu').init();
   new ProfileDropMenu('.button-profile_connected', '.connected-menu').init();
   new ProfileDropMenu('.currency-selection-header', '.currency-selection-drop-menu', 'currency-selection-header_active').init();
+
+  new ItemsActive('.items-group-wrapper', 'items_active').init();
 })
---
+++ src/js/modules/items-active.js
@@ -0,0 +1,43 @@
+export default class ItemsActive {
+  constructor(tiggersItems, activeClass = '') {
+    this.items = document.querySelectorAll(tiggersItems);
+    this.active = activeClass;
+    this.clickItems = false;
+  }
+
+  showItem() {
+    this.items.forEach(item => {
+      item.addEventListener('click', (e) => {
+
+        const allItems = item.parentElement.querySelectorAll('.item').length * 61 + 32;
+        const icon = item.querySelector('.items-group__icon');
+        if (e.target) {
+          e.preventDefault();
+        }
+
+        if (this.clickItems) {
+          this.clickItems = false;
+
+          item.parentElement.style.cssText = `
+          height: ${32}px;
+          overflow: hidden;
+          `;
+          icon.style.cssText = `transform: rotate(0deg);`;
+        } else {
+          this.clickItems = true;
+
+          item.parentElement.style.cssText = `
+          height: ${allItems}px;
+          overflow: auto;
+          `;
+          icon.style.cssText = `transform: rotate(90deg);`;
+        }
+
+      });
+    })
+  }
+
+  init() {
+    this.showItem();
+  }
+}
