d0a042d1895cf610c4dcc6d4fb13c061ff25b65f
Maksym <bilanych.max98009@gmail.com>
2022-06-29 12:26:07 +0300

    add fix menu

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,19 @@ import * as flsFunctions from "./modules/functions.js";
 flsFunctions.isWebp();
 
 import createLine from './modules/create-line.js';
-import profileDropMenu from "./modules/profile-drop-menu.js";
 import MoreMenuBtn from './modules/more-btn-menu.js';
 import Accordion from "./modules/accordion.js";
 import ChangeNameMoneyItem from "./modules/change-name-money-item.js";
 import ShowEditBlock from './modules/show-edit-block.js';
+import ProfileDropMenu from './modules/profile-drop-menu.js';
 
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
   createLine();
 
-  profileDropMenu('.button-profile');
+  new ProfileDropMenu('.button-profile', 'profile-drop-block_active', '.profile-drop-block').init();
+  new ProfileDropMenu('.currency-selection-header', 'show-menu', '.currency-selection-drop-menu').init();
 
   new MoreMenuBtn('.more-btn').init();
 
--- src/js/modules/profile-drop-menu.js
+++ src/js/modules/profile-drop-menu.js
@@ -1,55 +1,61 @@
-const profileDropMenu = () => {
-  const btns = document.querySelectorAll('.button-profile');
-  let intervalId;
+export default class ProfileDropMenu {
+  constructor(btnSelector, activeSelector, wrapperBlockSelector) {
+    this.btns = document.querySelectorAll(btnSelector);
+    this.active = activeSelector;
+    this.wrapperBlock = wrapperBlockSelector;
+    this.intervalId;
+  }
 
-  btns.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-      const dataBtn = e.currentTarget.getAttribute('data-path');
-      const menu = document.querySelector(`[data-target=${dataBtn}]`);
+  init() {
+    this.btns.forEach(btn => {
+      btn.addEventListener('click', (e) => {
+        const dataBtn = e.currentTarget.getAttribute('data-path');
+        const menu = document.querySelector(`[data-target=${dataBtn}]`);
 
-      for (let i = 0; i < btns.length; i++) {
-        btns[i].nextElementSibling.classList.remove('profile-drop-block_active');
-        intervalId = setTimeout(() => {
-          btns[i].classList.remove('open');
-        }, 0);
-      }
+        console.log(dataBtn, menu);
 
-      if (!btn.classList.contains('open')) {
-        menu.classList.add('profile-drop-block_active');
-
-        intervalId = setTimeout(() => {
-          btn.classList.add('open');
-        }, 0);
-      }
-
-      if (btn.classList.contains('open')) {
-        clearTimeout(intervalId);
-
-        menu.classList.remove('selected-blocks-list_active');
-
-        intervalId = setTimeout(() => {
-          btn.classList.remove('open');
-        }, 0);
-      }
-    })
-
-    
-  });
-
-  window.addEventListener('click', (e) => {
-    const target = e.target;
-
-    for(let i = 0; i < btns.length; i++) {
-      if (target && !target.matches('.profile-drop-block') && btns[i].classList.contains('open') && !target.closest("div[data-target]")) {
-        for (let i = 0; i < btns.length; i++) {
-          btns[i].nextElementSibling.classList.remove('profile-drop-block_active');
-          intervalId = setTimeout(() => {
-            btns[i].classList.remove('open');
+        for (let i = 0; i < this.btns.length; i++) {
+          this.btns[i].nextElementSibling.classList.remove(this.active);
+          this.intervalId = setTimeout(() => {
+            this.btns[i].classList.remove('open');
           }, 0);
         }
-      }
-    }
-  });
-}
 
-export default profileDropMenu;
+        if (!btn.classList.contains('open')) {
+          menu.classList.add(this.active);
+
+          this.intervalId = setTimeout(() => {
+            btn.classList.add('open');
+          }, 0);
+        }
+
+        if (btn.classList.contains('open')) {
+          clearTimeout(this.intervalId);
+
+          menu.classList.remove(this.active);
+
+          this.intervalId = setTimeout(() => {
+            btn.classList.remove('open');
+          }, 0);
+        }
+      })
+
+
+    });
+
+    window.addEventListener('click', (e) => {
+      const target = e.target;
+
+      for (let i = 0; i < this.btns.length; i++) {
+        if (target && !target.matches(this.wrapperBlock) && this.btns[i].classList.contains('open') && !target.closest("div[data-target]")) {
+          for (let i = 0; i < this.btns.length; i++) {
+            this.btns[i].nextElementSibling.classList.remove(this.active);
+            this.intervalId = setTimeout(() => {
+              this.btns[i].classList.remove('open');
+            }, 0);
+          }
+        }
+      }
+    });
+  }
+}
